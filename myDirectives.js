a60d45126871277d5da26ee3a311e33408d1398d
Zander Allen <ztrain01@gmail.com>
2015-02-26 20:17:23 -0700

    creating a pending directive

--- myDirectives.js
+++ myDirectives.js
@@ -1,5 +1,11 @@
 var app = angular.module('myDirectives', []);
 
 app.directive('mainDirective', function(){
-    
-  });
+    return {
+      restrict: 'AE',
+      scope: {},
+      link: function(){
+      //Code Here
+      }
+    }
+});
